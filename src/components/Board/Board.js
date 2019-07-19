7b9ad0088e18d39b251583edecce4ec7f6edfcc7
ki_blank <lbin7310@gmail.com>
2019-07-19 20:56:22 +0900

    Write Board Component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
+import Container from './Container';
 import './Board.css'
 
 class Board extends Component {
   constructor (props) {
     super (props)
-    this.state={
-      containerToggle: true
-    }
-  }
-
-  onCreateContainer = () => {
-    
-  }
-
-  onContainerToggle = () => {
-    const { containerToggle } = this.state;
-    this.setState({
-      containerToggle: !containerToggle
-    })
+    this.state={}
   }
 
   render() {
-    const { containers } = this.props;
-    const { containerToggle } = this.state;
-    const { onContainerToggle } = this; 
+    const { containers,
+            onAddContainer,
+            getCardTitleAndDescription,
+            boardId } = this.props;
     return (
       <div>
-        <div className="containers">
-          {containers.map( container => {
-            return (
-              <div className="container" key={container.id}>
-                {container.title}
-              </div>
-            )
-          })}
-          <div className="Add-Container" >
-            <div className="Add-a-list" onClick={onContainerToggle} style={{display: containerToggle ?  '' : 'none' }}>
-              Add a list
-            </div>
-            <div style={{display: containerToggle ? 'none' : '' }}>
-              <input className="container" placeholder="New List Title"/>
-              <button onClick={onContainerToggle}>Add List</button>
-            </div>
-          </div>
-        </div>
+        <Container 
+          boardId={boardId}
+          containers={containers}
+          onAddContainer={onAddContainer}
+          getCardTitleAndDescription={getCardTitleAndDescription}
+        />
       </div>
     )
   }
