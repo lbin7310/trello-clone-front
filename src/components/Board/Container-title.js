34526bb3875ff43c140aa01a233ca07d7da70081
ki_blank <lbin7310@gmail.com>
2019-07-27 10:46:38 +0900

    Create container-title component

---
+++ src/components/Board/Container-title.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { updateContainer } from '../../function/Container'
+
+class ContainerTitle extends Component {
+  constructor (props){
+    super (props)
+    this.state={
+      containerId: 0,
+      containerTitle: '',
+      toggle: false
+    }
+  }
+
+  componentDidMount() {
+    const { title, containerid } = this.props
+    this.setState({
+      containerTitle: title,
+      containerId: containerid
+    })
+  }
+  
+  onToggle = (e) => {
+    const { toggle } = this.state;
+    this.setState({
+      toggle: !toggle
+    })
+  }
+
+  onSubmit = (e) => {
+    const { toggle, containerId, containerTitle } = this.state;
+    e.preventDefault();
+    updateContainer(containerId, containerTitle);
+    this.setState({
+      toggle: !toggle
+    })
+  }
+
+  onModifyTitle = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    const { toggle, 
+            containerTitle,
+            containerId }=this.state;
+
+    const { onModifyTitle,
+            onSubmit,
+            onToggle }=this;
+
+    return (
+      <div>
+        <div className="container-title"
+          onClick={onToggle} style={{display: toggle ? 'none' : ''}}
+        >{containerTitle}
+        </div>
+        <div style={{display: toggle ? '' : 'none'}}>
+          <form 
+            id={containerId}
+            onSubmit={onSubmit}
+            >
+            <input 
+              type="text"
+              name="containerTitle"
+              value={containerTitle}
+              onChange={onModifyTitle}
+            />
+            <button type='submit'>수정</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ContainerTitle;
